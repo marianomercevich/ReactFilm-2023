38b5eade4186918940ece14a2d1cc4d392290396
Mariano Mercevich <marianomercevich@gmail.com>
2023-10-27 19:40:04 -0300

    TEST PROD

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,8 @@ const App = () => {
   return (
     <div>
       <h1>
-<<<<<<< HEAD
-        React PROD TEST!!!
-          </h1>
-=======
         react desde Uat!!!
       </h1>
->>>>>>> UAT
       <h3>
         Clon de Netflix.
       </h3>
